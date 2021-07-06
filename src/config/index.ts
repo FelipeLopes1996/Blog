7e974e6bae4b707821492bd254b794998c8bcb93
Felipe de Lima Lopes <felipe.lopes@pampa.compasso>
2021-07-06 10:16:07 -0300

    changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 export default {
   // url: 'http://localhost:1337/pages/?slug=',
   // url: 'https://api-teste-landing.herokuapp.com/pages/?slug=',
-  url: 'https://blog-test-one.herokuapp.com/graphql',
+  // url: 'https://blog-test-one.herokuapp.com/graphql',
   siteName: 'Felipe de Lima',
   defaultSlug: 'landing-page',
-  graphqlURL: 'http://127.0.0.1:1337/graphql',
+  graphqlURL: 'https://blog-test-one.herokuapp.com/graphql',
 };
